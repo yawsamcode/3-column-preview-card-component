f4c65c45ca26f7b922ffb29b186e14874932c088
yawsamcode <samuelacheampong108@gmail.com>
2024-03-25 18:27:48 +0000

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        LexendDeca: ["Lexend Deca", "sans-serif"],
+        BigShouldDisplay: ["Big Shoulders Display", "sans-serif"],
+      },
+
+      colors: {
+        BrightOrange: 'hsl(31, 77%, 52%)',
+        DarkCyan: 'hsl(184, 100%, 22%)',
+        VeryDarkCyan: 'hsl(179, 100%, 13%)',
+        VeryLightGray: 'hsl(0, 0%, 95%)', 
+        TransparentWhite: 'hsla(0, 0%, 100%, 0.75)'
+      }
+    },
+  },
+  plugins: [],
+}
